57dbe042aec17b406de0a2241111cf6b3e1e568b
AstralArchitect <154975712+AstralArchitect@users.noreply.github.com>
2024-01-13 22:02:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main()
     cout << "Bienvenue dans le programme de révision des tables de multiplications de Grégoire!" << endl;
 
     cout << "Quelle table de multiplication veux-tu reviser ? " << endl;
+    bool used[13];
     auto proposition{0};
     auto reponse{0};
     auto mauvaiseReponses{0};
@@ -25,7 +26,12 @@ int main()
 
     for (size_t i = 0; i < 12 /*c'est nombre de fois que tu veux répéter cette boucle */; i++)
     {
-        int multiplieur{random(1, 12)};
+        int multiplieur{random(0, 12)};
+        while(used[multiplieur])
+        {
+        multiplieur = random(0, 12);
+        }
+        used[multiplieur] = true;
         //poser la question à l'utilisateur
         int bonneReponse{proposition * multiplieur};
         cout << "Combien font " << proposition << " X " << multiplieur << " ?" << endl;
